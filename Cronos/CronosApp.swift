6bf6ecf5d07c4b806a00fc9f9a791a84afcf566d
Moisés Neto <ntocampos@gmail.com>
2025-10-11 16:44:23 -0300

    feat: adds new models to app setup

--- Cronos/CronosApp.swift
+++ Cronos/CronosApp.swift
@@ -12,7 +12,8 @@ import SwiftUI
 struct CronosApp: App {
   var sharedModelContainer: ModelContainer = {
     let schema = Schema([
-      Item.self
+      Deadline.self,
+      Category.self
     ])
     let modelConfiguration = ModelConfiguration(
       schema: schema,
