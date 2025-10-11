ba0069c7d8587057d7cda79c85db399ac7fcfe15
Moisés Neto <ntocampos@gmail.com>
2025-10-11 16:42:26 -0300

    feat: formats initial code

--- Cronos/CronosApp.swift
+++ Cronos/CronosApp.swift
@@ -5,28 +5,34 @@
 //  Created by Moisés Neto on 11/10/25.
 //
 
-import SwiftUI
 import SwiftData
+import SwiftUI
 
 @main
 struct CronosApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+  var sharedModelContainer: ModelContainer = {
+    let schema = Schema([
+      Item.self
+    ])
+    let modelConfiguration = ModelConfiguration(
+      schema: schema,
+      isStoredInMemoryOnly: false
+    )
 
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-        .modelContainer(sharedModelContainer)
+    do {
+      return try ModelContainer(
+        for: schema,
+        configurations: [modelConfiguration]
+      )
+    } catch {
+      fatalError("Could not create ModelContainer: \(error)")
     }
+  }()
+
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+    .modelContainer(sharedModelContainer)
+  }
 }
